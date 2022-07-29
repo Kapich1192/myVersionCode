8de44b080909ba0ec4cd42762aab35ea7805a32a
kapich1192 <kapich1192@yandex.ru>
2022-07-29 12:05:38 +0300

    dashboard developing

--- src/main/java/com/code/controllers/dashboard/dashboardController/DashboardController.java
+++ src/main/java/com/code/controllers/dashboard/dashboardController/DashboardController.java
@@ -1,10 +1,15 @@
 package com.code.controllers.dashboard.dashboardController;
 
+import com.code.models.user.User;
 import com.code.repo.user.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class DashboardController {
@@ -13,8 +18,25 @@ public class DashboardController {
 /*GET*/
     @GetMapping("/dashboard")
     public String getDashboard(Model model) {
+        /*Meta*/
         model.addAttribute("title", "Dashboard");
         model.addAttribute("Description", "Your working place");
+        /*User*/
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = auth.getPrincipal();
+        User user = userRepo.findByUsername(auth.getName());
+        /*Level*/
+        model.addAttribute("user_level",user.getLevel());
+        model.addAttribute("user_exp", user.getExperience());
+        /*Info*/
+        model.addAttribute("user_email", user.getEmail());
+        model.addAttribute("user_tel", user.getTel());
+        /*Points*/
+        model.addAttribute("peer_points",user.getPeerPoints());
+        /*Skills*/
+        model.addAttribute("html_level",user.getHtmlSkill());
+        model.addAttribute("js_level",user.getJavaScriptSkill());
+        model.addAttribute("java_level", user.getJavaSkill());
         return "pages/dashboard/dashboard";
     }
 }
--- src/main/java/com/code/models/user/User.java
+++ src/main/java/com/code/models/user/User.java
@@ -6,18 +6,32 @@ import java.util.Set;
 @Entity
 @Table(name = "usr")
 public class User {
-    /*fields*/
+    /*=============== FIELDS ===============*/
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    /*Profile*/
     private String username;
     private String password;
     private boolean active;
+    private String email;
+    private String tel;
+    /*level*/
+    private int level = 0;
+    private int experience = 0;
+    /*points*/
+    private int peerPoints = 0;
+    /*Role*/
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER )
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
-    /*constructors*/
+
+    /*skills*/
+    private int htmlSkill = 0;
+    private int javaScriptSkill= 0;
+    private int javaSkill = 0;
+    /*=============== CONSTRUCTORS ===============*/
     public User() {}
 
     public User(Long id, String username, String password) {
@@ -31,17 +45,32 @@ public class User {
         this.password = password;
     }
 
-    /*getters*/
+    /*=============== GETTERS ===============*/
     public Long getId() { return id; }
     public String getUsername() { return username; }
     public String getPassword() { return password; }
+    public String getEmail() { return email; }
+    public String getTel() { return tel; }
     public boolean isActive() { return active; }
     public Set<Role> getRoles() { return roles; }
-
-    /*setters*/
+    public int getPeerPoints() { return peerPoints; }
+    public int getLevel() { return level;}
+    public int getExperience() { return experience; }
+    public int getHtmlSkill() { return htmlSkill; }
+    public int getJavaScriptSkill() { return javaScriptSkill; }
+    public int getJavaSkill() { return javaSkill; }
+    /*=============== SETTERS ===============*/
     public void setId(Long id) { this.id = id; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
+    public void setEmail(String email) { this.email = email; }
+    public void setTel(String tel) { this.tel = tel; }
     public void setActive(boolean active) { this.active = active; }
     public void setRoles(Set<Role> roles) { this.roles = roles; }
+    public void setPeerPoints(int peerPoints) { this.peerPoints = peerPoints; }
+    public void setLevel(int level) { this.level = level; }
+    public void setExperience(int experience) { this.experience = experience; }
+    public void setHtmlSkill(int htmlSkill) { this.htmlSkill = htmlSkill; }
+    public void setJavaScriptSkill(int javaScriptSkill) { this.javaScriptSkill = javaScriptSkill; }
+    public void setJavaSkill(int javaSkill) { this.javaSkill =javaSkill; }
 }
